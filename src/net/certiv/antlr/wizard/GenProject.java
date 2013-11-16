f8cf2a42ab00c29912de10af79a9e4f4fcf04a95
GRosenberg <gbrosenberg85@gmail.com>
2013-11-16 15:36:43 -0800

    Updated symbol table design

--- src/net/certiv/antlr/wizard/GenProject.java
+++ src/net/certiv/antlr/wizard/GenProject.java
@@ -53,11 +53,12 @@ public class GenProject {
 
 	private static final Pattern ruleName = Pattern.compile("void enter(\\w*)\\(");
 	private static final IOFileFilter antlrJar = new WildcardFileFilter("antlr-4*.jar");
-	private static final String AntlrJarName = "antlr-4.0b2-complete.jar";
+	private static final String AntlrJarName = "antlr-4.0-complete.jar";
 	private static final String javaDefalutHome = "C:/Program Files/Java/jre7";
 	private static final String templateDir = "net/certiv/antlr/wizard/templates";
 
 	private static final FilenameFilter filter = new FilenameFilter() {
+
 		public boolean accept(File dir, String name) {
 			if (name.toLowerCase().endsWith(".json")) return true;
 			return false;
@@ -76,6 +77,8 @@ public class GenProject {
 	private String dstGenerator;
 	private String dstParser;
 	private String dstGen;
+	private String dstSymbol;
+	private String dstTypes;
 	private String dstUtil;
 
 	public static void main(String[] args) {
@@ -212,6 +215,9 @@ public class GenProject {
 							config.getAntlrJarPathName(), config.getJavaPath(), config.getGrammarName());
 					createUtils(config.getPackageName());
 
+					createSymbols(config.getPackageName());
+					createTypes(config.getPackageName());
+
 					lastError = "Failure in processing parser creation";
 					createParser(config.getPackageName(), config.getGrammarName());
 
@@ -275,12 +281,16 @@ public class GenProject {
 		dstParser = concat(dstBase, "parser");
 		dstGen = concat(dstBase, "parser", "gen");
 		dstGenerator = concat(dstBase, "generator");
+		dstSymbol = concat(dstBase, "symbol");
+		dstTypes = concat(dstBase, "types");
 		dstUtil = concat(dstBase, "util");
 
 		return createDirs(dstBase)
 				&& createDirs(dstDescriptors)
 				&& createDirs(dstGen)
 				&& createDirs(dstGenerator)
+				&& createDirs(dstSymbol)
+				&& createDirs(dstTypes)
 				&& createDirs(dstUtil);
 	}
 
@@ -318,14 +328,21 @@ public class GenProject {
 		createFirstWalkerPhase(packageName, grammarName, "01");
 		createWalkerPhase(packageName, grammarName, "02", genNames);
 		createWalkerPhase(packageName, grammarName, "03", genNames);
-		createWalkerPhase(packageName, grammarName, "10", genNames);
+		createPhaseState(packageName);
 	}
 
 	private void createFirstWalkerPhase(String packageName, String grammarName, String phaseNumber) throws IOException {
 		STGroup group = new STGroupFile(concatAsClassPath(templateDir, "PhaseClasses.stg"));
+
+		// use grammar name to define presumptive first rule name
+		String genName = grammarName.substring(0, 1).toUpperCase();
+		genName += grammarName.substring(1).toLowerCase();
+
 		ST st = group.getInstanceOf("FirstPhaseClass");
 		st.add("packageName", packageName);
 		st.add("grammarName", grammarName);
+		st.add("genName", genName);
+		st.add("phaseNumber", phaseNumber);
 		String result = st.render();
 		config.writeFile(concat(dstConverter, grammarName + "Phase" + phaseNumber + ".java"), result);
 	}
@@ -348,6 +365,7 @@ public class GenProject {
 
 		st = group.getInstanceOf("PhaseClassImport2");
 		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
 		for (String genName : genNames) {
 			st.add("genName", genName);
 			result.append(st.render());
@@ -360,47 +378,38 @@ public class GenProject {
 		st.add("phaseNumber", phaseNumber);
 		result.append(st.render());
 
-		if (phaseNumber.equals("01")) {
-			st = group.getInstanceOf("PhaseClassInitialConstructor");
-			st.add("packageName", packageName);
-			st.add("grammarName", grammarName);
-			st.add("phaseNumber", phaseNumber);
-			result.append(st.render());
-		}
-
 		st = group.getInstanceOf("PhaseClassConstructor");
 		st.add("packageName", packageName);
 		st.add("grammarName", grammarName);
 		st.add("phaseNumber", phaseNumber);
 		result.append(st.render());
 
-		if (phaseNumber.equals("01")) {
-			st = group.getInstanceOf("PhaseClassComments");
+		st = group.getInstanceOf("PhaseClassEnterMethod");
+		for (String genName : genNames) {
+			st.add("genName", genName);
 			result.append(st.render());
+			st.remove("genName");
 		}
 
-		if (phaseNumber.equals("01")) {
-			st = group.getInstanceOf("PhaseClassEnterMethod");
-			for (String genName : genNames) {
-				st.add("genName", genName);
-				result.append(st.render());
-				st.remove("genName");
-			}
-		}
-
-		if (!phaseNumber.equals("01")) {
-			st = group.getInstanceOf("PhaseClassExitMethod");
-			for (String genName : genNames) {
-				st.add("genName", genName);
-				result.append(st.render());
-				st.remove("genName");
-			}
+		st = group.getInstanceOf("PhaseClassExitMethod");
+		for (String genName : genNames) {
+			st.add("genName", genName);
+			result.append(st.render());
+			st.remove("genName");
 		}
 
 		st = group.getInstanceOf("PhaseClassTrailer");
 		result.append(st.render());
 
 		config.writeFile(concat(dstConverter, grammarName + "Phase" + phaseNumber + ".java"), result.toString());
+	}
+
+	private void createPhaseState(String packageName) throws IOException {
+		STGroup group = new STGroupFile(concatAsClassPath(templateDir, "PhaseClasses.stg"));
+		ST st = group.getInstanceOf("PhaseStateClass");
+		st.add("packageName", packageName);
+		String result = st.render();
+		config.writeFile(concat(dstConverter, "PhaseState.java"), result);
 	}
 
 	public void createBaseClasses(String packageName, String grammarName) throws IOException {
@@ -434,15 +443,65 @@ public class GenProject {
 		st.add("grammarName", grammarName);
 		result = st.render();
 		config.writeFile(concat(dstParser, grammarName + "Lexer.g4"), result);
+
+		st = group.getInstanceOf("LexerAdapterClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstParser, "LexerAdaptor.java"), result);
+
+		st = group.getInstanceOf("LexerHelperClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstParser, "LexerHelper.java"), result);
+
+		st = group.getInstanceOf("ErrorListenerClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstParser, grammarName + "ErrorListener.java"), result);
+
+		st = group.getInstanceOf("LexerErrorStrategyClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstParser, grammarName + "LexerErrorStrategy.java"), result);
+
+		st = group.getInstanceOf("ParserErrorStrategyClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstParser, grammarName + "ParserErrorStrategy.java"), result);
+
+		st = group.getInstanceOf("TokenClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstParser, grammarName + "Token.java"), result);
+
+		st = group.getInstanceOf("TokenFactoryClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstParser, grammarName + "TokenFactory.java"), result);
+
 	}
 
 	public void createGenerator(String packageName, String grammarName) throws IOException {
 		STGroup group = new STGroupFile(concatAsClassPath(templateDir, "FileGen.stg"));
+
 		ST st = group.getInstanceOf("OutputFileGen");
 		st.add("packageName", packageName);
 		st.add("grammarName", grammarName);
 		String result = st.render();
 		config.writeFile(concat(dstGenerator, grammarName + "FileGen.java"), result);
+
+		st = group.getInstanceOf("IOProcessorClass");
+		st.add("packageName", packageName);
+		st.add("grammarName", grammarName);
+		result = st.render();
+		config.writeFile(concat(dstGenerator, "IOProcessor.java"), result);
 	}
 
 	public void createDescritors(String packageName, String grammarName, List<String> descriptorNames)
@@ -489,6 +548,58 @@ public class GenProject {
 		config.writeFile(concat(dstParser, grammarName + "Tool.bat"), result);
 	}
 
+	public void createSymbols(String packageName) throws IOException {
+		STGroup group = new STGroupFile(concatAsClassPath(templateDir, "Symbol.stg"));
+		ST st = group.getInstanceOf("SymbolClass");
+		st.add("packageName", packageName);
+		String result = st.render();
+		config.writeFile(concat(dstSymbol, "Symbol.java"), result);
+
+		st = group.getInstanceOf("ScopeClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstSymbol, "Scope.java"), result);
+
+		st = group.getInstanceOf("SymbolTableClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstSymbol, "SymbolTable.java"), result);
+	}
+
+	public void createTypes(String packageName) throws IOException {
+		STGroup group = new STGroupFile(concatAsClassPath(templateDir, "Types.stg"));
+		ST st = group.getInstanceOf("ValueTypeClass");
+		st.add("packageName", packageName);
+		String result = st.render();
+		config.writeFile(concat(dstTypes, "ValueType.java"), result);
+
+		st = group.getInstanceOf("StyleTypeClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstTypes, "StyleType.java"), result);
+
+		st = group.getInstanceOf("ContentClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstTypes, "Content.java"), result);
+
+		st = group.getInstanceOf("ScopeTypeClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstTypes, "ScopeType.java"), result);
+
+		st = group.getInstanceOf("StmtTypeClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstTypes, "StmtType.java"), result);
+
+		st = group.getInstanceOf("OpClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstTypes, "Op.java"), result);
+
+	}
+
 	public void createUtils(String packageName) throws IOException {
 		STGroup group = new STGroupFile(concatAsClassPath(templateDir, "Utils.stg"));
 		ST st = group.getInstanceOf("LogClass");
@@ -500,5 +611,10 @@ public class GenProject {
 		st.add("packageName", packageName);
 		result = st.render();
 		config.writeFile(concat(dstUtil, "Reflect.java"), result);
+
+		st = group.getInstanceOf("StringsClass");
+		st.add("packageName", packageName);
+		result = st.render();
+		config.writeFile(concat(dstUtil, "Strings.java"), result);
 	}
 }
